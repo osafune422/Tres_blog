c394ba27d9fd81d5361a1d11154e3f7995092d2d
EC2 Default User <ec2-user@ip-172-31-38-82.us-east-2.compute.internal>
2018-05-15 02:38:18 +0000

    orderAdd

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :posts
+    has_many :posts, -> { order(updated_at: :desc) }
     
     # nameのエラー条件
     validates :name, {presence: true}
     # login_idのエラー条件
     validates :login_id, {presence: true, uniqueness: true}
+    
 end
