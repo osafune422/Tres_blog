2a49ab50cd71db1e562872971d12da76582f833c
EC2 Default User <ec2-user@ip-172-31-38-82.us-east-2.compute.internal>
2018-05-15 02:29:24 +0000

    v2.2(has_manyAdd)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    
     validates :user_id, {presence: true}
     validates :title, {presence: true, length: {maximum: 100}}
     validates :contents, {presence: true, length: {maximum: 800, minimum: 5}}
   
-    def user
-        return User.find_by(id: self.user_id)
-    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :posts
     
     # nameのエラー条件
     validates :name, {presence: true}
     # login_idのエラー条件
     validates :login_id, {presence: true, uniqueness: true}
-    
-    def posts
-        @post=Post.where(user_id: self.id)
-        return @post.all.order("created_at DESC")
-    end
 end
